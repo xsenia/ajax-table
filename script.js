097ba12005575b8d0aaa1a7434cfeaf9ccce7bbb
xsenia <belkinasan@yandex.ru>
2020-01-26 19:59:12 +0500

    все поля в одну строку

--- script.js
+++ script.js
@@ -1,33 +1,36 @@
 document.addEventListener('DOMContentLoaded', () => {
     'use strict';
 
-    const dataWrap = document.getElementById('dataWrap');
+    const dataWrap = document.getElementById('data-wrap');
+    const filterHeader = document.getElementById('filter-header');
 
-    const request = new XMLHttpRequest();    
-      
-    request.open('GET', 'http://jsonplaceholder.typicode.com/users', true); //настроить ajax запрос. Первый параметр - метод отправки сообщения GET, Post. Второй - URL -  куда будет отправлен запрос, это может  быть локальный адрес. Третий -  asinc = true  по умолчанию.
-    
-    request.setRequestHeader('Content-type', 'application/json'); //мы осуществляем http запрос, а он состоит из заголовка и боди. Тут устнавливается заголовок. Первый парам - имя, второй - его значение
 
-    request.send(); //метод открывает соединение и отправляет запрос. и параметром передает данные на сервер
+    const request = new XMLHttpRequest();
+    request.open('GET', 'http://jsonplaceholder.typicode.com/users', true);
+    request.setRequestHeader('Content-type', 'application/json');
+    request.send();
+
+
     
     request.addEventListener('readystatechange', () => {
          
-          if (request.readyState === 4 && request.status === 200) {
-                //переводим из JSON в объект: 
-                const data = JSON.parse(request.responseText);                
+      if (request.readyState === 4 && request.status === 200) {
 
-                for (let i = 0; i < data.length; i++) {
+            const data = JSON.parse(request.responseText);
+
+            const headerData = data[0];
+            for (let key in headerData) {
+                  filterHeader.innerHTML += '<div class="keys">' + key + '</div>';
+            }  
+
+            for (let i = 0; i < data.length; i++) {
                   let obj = data[i];
                   for (let key in obj) {
-                        dataWrap.innerHTML += '<tr><td>' + key + '</td><td>' + obj[key] + '</td></tr>';
-                  }
-                  
-                }
-           
-          }  else {
-            dataWrap.innerHTML = 'Ошибка';
-          }                      
+                        dataWrap.innerHTML += '<div class="value">' + obj[key] + '<div>';
+                  }                  
+            }
+
+      }                    
     });
     
     
